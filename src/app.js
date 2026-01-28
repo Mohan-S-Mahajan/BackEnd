eb245aabab671224f8f95642a4b61a028bdd45b9
Mohan-S-Mahajan <mohan.mahajann@gmail.com>
2026-01-28 20:47:09 +0530

    set up configuration of controllers and routers and demo userRouter register

--- src/app.js
+++ src/app.js
@@ -7,17 +7,24 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
 }))
-
 app.use(express.json({
     limit: '16kb'
 }))
-
 app.use(express.urlencoded({
     extended: true,
     limit: '16kb',
 }))
 app.use(express.static("public"))
-
 app.use(cookieParser())
+
+
+// route import
+import userRouter from "./routes/user.routes.js";
+
+
+// route declare
+app.use("/api/v1/users", userRouter)
+// http://localhost:8000/api/v1/users/register
+
 
 export { app }
